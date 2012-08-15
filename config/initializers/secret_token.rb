9a1272911a7ae81099851f2f3dffe7380d84a808
Matt Patterson <matt@reprocessed.org>
2012-08-15 19:27:25 +0200

    bare Rails 3.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoutingWorkshop::Application.config.secret_token = '8ca6c68b06872952515507ca99ca0ce81a2092b26cd6cea9c5e703ce01b3ccc98a6a4e66128d84c89680d9aceed8a30793ae869c4232a14c4bd83fceee4dbc91'
---
+++ config/routes.rb
@@ -0,0 +1,2 @@
+RoutingWorkshop::Application.routes.draw do
+end
